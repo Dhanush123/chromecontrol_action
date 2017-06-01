c5517e449ed0be022614855a86e26f1df4ca4f25
Dhanush123 <dhanush.patel@ymail.com>
2017-05-31 23:55:27 -0700

    init things
    nothing here…yet

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+const App = require('actions-on-google').ApiAiApp;
+
+// [START YourAction]
+exports.chromeControl = (request, response) => {
+const app = new App({request, response});
+console.log('Request headers: ' + JSON.stringify(request.headers));
+console.log('Request body: ' + JSON.stringify(request.body));
+
+// Fulfill action business logic
+function responseHandler (app) {
+  // Complete your fulfillment logic and send a response
+  app.ask('Hello, World!');
+}
+
+const actionMap = new Map();
+actionMap.set('<API.AI_action_name>', responseHandler);
+
+app.handleRequest(actionMap);
+};
