ba5f7a7eee96e3d5b137ad02edbcb3c322f12ca5
Dhanush123 <dhanush.patel@ymail.com>
2017-06-12 18:59:03 -0700

    asdasd
    asd

--- index.js
+++ index.js
@@ -2,22 +2,27 @@
 
 process.env.DEBUG = "actions-on-google:*";
 const App = require("actions-on-google").ApiAiApp;
+const express = require("express");
 
+var server = express();
 // [START YourAction]
-exports.chromeControl = (request, response) => {
-const app = new App({request, response});
-console.log("Request headers: " + JSON.stringify(request.headers));
-console.log("Request body: " + JSON.stringify(request.body));
+server.post('/hook', function(req, res) {
+  const app = new App({req, res});
+  console.log("Request headers: " + JSON.stringify(request.headers));
+  console.log("Request body: " + JSON.stringify(request.body));
 
-// Fulfill action business logic
-function permsRequest (app) {
-  const permission = app.SupportedPermissions.NAME;
-  app.askForPermission("To link your device to your Google Chrome browser", permission);
-}
+  // Fulfill action business logic
+  function permsRequest (app) {
+    const permission = app.SupportedPermissions.NAME;
+    app.askForPermission("To link your device to your Google Chrome browser", permission);
+  }
 
+  const actionMap = new Map();
+  actionMap.set("permsaction", permsRequest);
 
-const actionMap = new Map();
-actionMap.set("permsaction", permsRequest);
+  app.handleRequest(actionMap);
+});
 
-app.handleRequest(actionMap);
-};
+server.listen(8000, function() {
+  console.log('Server listening');
+});
