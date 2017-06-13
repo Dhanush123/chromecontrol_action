9a91c0186ad5d7a2319cc8a28af1587715b48f57
Dhanush123 <dhanush.patel@ymail.com>
2017-06-12 21:21:42 -0700

    sadsads
    dasd

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ server.post('/hook', function(req, res) {
   app.handleRequest(actionMap);
 });
 
+server.get('/auth/google',
+  passport.authenticate('google', { scope:
+  	['https://www.googleapis.com/auth/plus.login',
+  	, 'https://www.googleapis.com/auth/plus.profile.emails.read' ] }
+));
+
+server.get('/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
+
 server.listen(8000, function() {
   console.log('Server listening');
 });
