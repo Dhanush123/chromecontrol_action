b72c2be3c98057cfcfb5270ff39b327f3850b558
Dhanush123 <dhanush.patel@ymail.com>
2017-06-12 21:28:51 -0700

    sadasdgdf
    asdsda

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ process.env.DEBUG = "actions-on-google:*";
 const App = require("actions-on-google").ApiAiApp;
 const express = require("express");
 const passport = require("passport");
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
 var server = express();
 // [START YourAction]
-server.post('/hook', function(req, res) {
+server.post("/hook", function(req, res) {
   const app = new App({req, res});
   console.log("Request headers: " + JSON.stringify(request.headers));
   console.log("Request body: " + JSON.stringify(request.body));
@@ -25,18 +25,26 @@ server.post('/hook', function(req, res) {
   app.handleRequest(actionMap);
 });
 
-server.get('/auth/google',
-  passport.authenticate('google', { scope:
-  	['https://www.googleapis.com/auth/plus.login',
-  	, 'https://www.googleapis.com/auth/plus.profile.emails.read' ] }
+server.get("/auth/google",
+  passport.authenticate("google", { scope:
+  	["https://www.googleapis.com/auth/plus.login",
+  	, "https://www.googleapis.com/auth/plus.profile.emails.read" ] }
 ));
 
-server.get('/auth/google/callback',
-    passport.authenticate( 'google', {
-        successRedirect: '/auth/google/success',
-        failureRedirect: '/auth/google/failure'
+server.get("/auth/google/callback",
+    passport.authenticate( "google", {
+        successRedirect: "/auth/google/success",
+        failureRedirect: "/auth/google/failure"
 }));
 
+server.get("auth/google/success",function(){
+  console.log("login success!!!!");
+});
+
+server.get("auth/google/failure",function(){
+  console.log("login failure!!!!");
+});
+
 server.listen(8000, function() {
-  console.log('Server listening');
+  console.log("Server listening");
 });
