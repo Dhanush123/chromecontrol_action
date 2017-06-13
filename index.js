015f59a073b7b56e7c21fbec87bbd5abbb7a300f
Dhanush123 <dhanush.patel@ymail.com>
2017-06-13 16:48:17 -0700

    sadas
    dasd

--- index.js
+++ index.js
@@ -7,25 +7,20 @@ const bodyParser = require('body-parser');
 
 var server = express();
 server.use(bodyParser.json());
-// [START YourAction]
+
 server.post("/", function(req, res) {
   const app = new App({req, res});
-  const actionMap = new Map();
-  actionMap.set("permsaction", permsRequest);
-  app.handleRequest(actionMap);
   console.log("Request headers: " + JSON.stringify(req.headers));
   console.log("Request body: " + JSON.stringify(req.body));
   console.log("req.body.result.action: "  + req.body.result.action);
   console.log("req.body.result.action == permsaction ? " + (req.body.result.action == "permsaction"));
-  // Fulfill action business logic
-  // if(req.body.result.action == "permsaction"){
-  //   permsRequest(app);
-  // }
+  const actionMap = new Map();
+  actionMap.set("permsaction", permsRequest);
+  app.handleRequest(actionMap);
+
   function permsRequest (app) {
     console.log("got in func???");
     app.tell("webhook works!!!");
-    // const permission = app.SupportedPermissions.NAME;
-    // app.askForPermission("To link your device to your Google Chrome browser", permission);
   }
 });
 
