4098482a9bfb8b07167aec04d68606cff0c88110
Dhanush123 <dhanush.patel@ymail.com>
2017-06-12 18:18:37 -0700

    sadasd
    sadasd

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-'use strict';
+"use strict";
 
-process.env.DEBUG = 'actions-on-google:*';
-const App = require('actions-on-google').ApiAiApp;
+process.env.DEBUG = "actions-on-google:*";
+const App = require("actions-on-google").ApiAiApp;
 
 // [START YourAction]
 exports.chromeControl = (request, response) => {
 const app = new App({request, response});
-console.log('Request headers: ' + JSON.stringify(request.headers));
-console.log('Request body: ' + JSON.stringify(request.body));
+console.log("Request headers: " + JSON.stringify(request.headers));
+console.log("Request body: " + JSON.stringify(request.body));
 
 // Fulfill action business logic
-function responseHandler (app) {
-  // Complete your fulfillment logic and send a response
-  app.ask('Hello, World!');
+function permsRequest (app) {
+  const permission = app.SupportedPermissions.NAME;
+  app.askForPermission("To link your device to your Google Chrome browser", permission);
 }
 
+
 const actionMap = new Map();
-actionMap.set('<API.AI_action_name>', responseHandler);
+actionMap.set("permsaction", permsRequest);
 
 app.handleRequest(actionMap);
 };
