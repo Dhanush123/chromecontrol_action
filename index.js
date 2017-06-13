4569b0df7e7dbdf7096bb2d9a715a5e1b00ce837
Dhanush123 <dhanush.patel@ymail.com>
2017-06-13 16:36:01 -0700

    sdfsdf
    sdfsdf

--- index.js
+++ index.js
@@ -10,46 +10,24 @@ server.use(bodyParser.json());
 // [START YourAction]
 server.post("/", function(req, res) {
   const app = new App({req, res});
+  const actionMap = new Map();
+  actionMap.set("permsaction", permsRequest);
+  app.handleRequest(actionMap);
   console.log("Request headers: " + JSON.stringify(req.headers));
   console.log("Request body: " + JSON.stringify(req.body));
   console.log("req.body.result.action: "  + req.body.result.action);
   console.log("req.body.result.action == permsaction ? " + (req.body.result.action == "permsaction"));
   // Fulfill action business logic
-  if(req.body.result.action == "permsaction"){
-    permsRequest(app);
-  }
+  // if(req.body.result.action == "permsaction"){
+  //   permsRequest(app);
+  // }
   function permsRequest (app) {
     console.log("got in func???");
     app.tell("webhook works!!!");
     // const permission = app.SupportedPermissions.NAME;
     // app.askForPermission("To link your device to your Google Chrome browser", permission);
   }
-  //
-  // const actionMap = new Map();
-  // actionMap.set("permsaction", permsRequest);
-  //
-  // app.handleRequest(actionMap);
 });
-
-// server.get("/auth/google",
-//   passport.authenticate("google", { scope:
-//   	["https://www.googleapis.com/auth/plus.login",
-//   	, "https://www.googleapis.com/auth/plus.profile.emails.read" ] }
-// ));
-//
-// server.get("/auth/google/callback",
-//     passport.authenticate( "google", {
-//         successRedirect: "/auth/google/success",
-//         failureRedirect: "/auth/google/failure"
-// }));
-//
-// server.get("auth/google/success",function(){
-//   console.log("login success!!!!");
-// });
-//
-// server.get("auth/google/failure",function(){
-//   console.log("login failure!!!!");
-// });
 
 server.listen(process.env.PORT || 8000, function() {
   console.log("Server listening");
