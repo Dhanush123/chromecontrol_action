5cc0fed438d8a531d72408f13877aa59972e5250
Dhanush123 <dhanush.patel@ymail.com>
2017-06-14 22:29:35 -0700

    asdasd
    salads

--- index.js
+++ index.js
@@ -2,18 +2,15 @@
 
 process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').ApiAiApp;
-var firebase = require('firebase');
-var fireApp = firebase.initializeApp({
-    apiKey: "AIzaSyAkpiEWoAPAca8MsXv26oZPflZWlHABc0I",
-    authDomain: "chromecontrol-77635.firebaseapp.com",
-    databaseURL: "https://chromecontrol-77635.firebaseio.com",
-    projectId: "chromecontrol-77635",
-    storageBucket: "chromecontrol-77635.appspot.com",
-    messagingSenderId: "258067669794"
-});
 var env = require('node-env-file');
-// const admin = require("firebase-admin");
-// const serviceAccount = require("./firebaseadmin.json");
+
+const admin = require("firebase-admin");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://chromecontrol-77635.firebaseio.com"
+});
+const serviceAccount = require("./firebaseadmin.json");
+var db = admin.database();
 
 // var google = require('googleapis');
 // var plus = google.plus('v1');
@@ -36,21 +33,15 @@ exports.chromeControl = (request, response) => {
   var user = app.getUser();
   console.log("user: "+JSON.stringify(user));
 
-  // Build Firebase credential with the Google ID token.
-  var credential = firebase.auth.GoogleAuthProvider.credential(user.accessToken);
-
-  // Sign in with credential from the Google user.
-  firebase.auth().signInWithCredential(credential).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    console.log("firebase returned...: " + JSON.stringify(error));
-    // ...
+  var ref = db.ref("/users");
+  ref.once("value")
+  .then(function(snapshot) {
+    var hasAccount = snapshot.hasChild(user.emails.value);
+    if(!hasAccount){
+      ref.set({ name: user.displayName, email: user.emails.value });
+    }
   });
+
   // oauth2Client.setCredentials({
   //   access_token: user.accessToken
   // });
@@ -76,38 +67,3 @@ exports.chromeControl = (request, response) => {
   app.handleRequest(actionMap);
 };
 // [END YourAction]
-
-// "use strict";
-//
-// process.env.DEBUG = "actions-on-google:*";
-// const App = require("actions-on-google").ApiAiApp;
-// // const express = require("express");
-// // const bodyParser = require('body-parser');
-// //
-// // var server = express();
-// // server.use(bodyParser.json());
-//
-// // server.post("/", function(req, res) {
-//
-// exports.chromeControl = (req, res) => {
-//   const app = new App({req, res});
-//   console.log("res: " + res);
-//   console.log("Request headers: " + JSON.stringify(req.headers));
-//   console.log("Request body: " + JSON.stringify(req.body));
-//   console.log("req.body.result.action: "  + req.body.result.action);
-//   console.log("req.body.result.action == permsaction ? " + (req.body.result.action == "permsaction"));
-//
-//   function permsRequest (app) {
-//     console.log("got in func???");
-//     app.tell("webhook works!!!");
-//   }
-//
-//   let actionMap = new Map();
-//   actionMap.set("permsaction", permsRequest);
-//   app.handleRequest(actionMap);
-// };
-// // });
-//
-// // server.listen(process.env.PORT || 8000, function() {
-// //   console.log("Server listening");
-// // });
