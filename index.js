a86d906a2c06b426c2007d987ef12a45e584404c
Dhanush123 <dhanush.patel@ymail.com>
2017-06-14 20:48:04 -0700

    gitignore added
    oath2 also working

--- index.js
+++ index.js
@@ -1,34 +1,93 @@
-"use strict";
+'use strict';
 
-process.env.DEBUG = "actions-on-google:*";
-const App = require("actions-on-google").ApiAiApp;
-// const express = require("express");
-// const bodyParser = require('body-parser');
-//
-// var server = express();
-// server.use(bodyParser.json());
+process.env.DEBUG = 'actions-on-google:*';
+const App = require('actions-on-google').ApiAiApp;
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebaseadmin.json");
+//dsfsdfs
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_URL
+});
 
-// server.post("/", function(req, res) {
+var google = require('googleapis');
+var plus = google.plus('v1');
+var OAuth2 = google.auth.OAuth2;
 
-exports.chromeControl = (req, res) => {
-  const app = new App({req, res});
-  console.log("res: " + res);
-  console.log("Request headers: " + JSON.stringify(req.headers));
-  console.log("Request body: " + JSON.stringify(req.body));
-  console.log("req.body.result.action: "  + req.body.result.action);
-  console.log("req.body.result.action == permsaction ? " + (req.body.result.action == "permsaction"));
+var oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URL
+);
 
-  function permsRequest (app) {
-    console.log("got in func???");
-    app.tell("webhook works!!!");
+ env(__dirname + '/.env');
+
+// [START YourAction]
+exports.chromeControl = (request, response) => {
+  const app = new App({request, response});
+  console.log('Request headers: ' + JSON.stringify(request.headers));
+  console.log('Request body: ' + JSON.stringify(request.body));
+
+  var user = app.getUser();
+  console.log("user: "+JSON.stringify(user));
+
+  oauth2Client.setCredentials({
+    access_token: user.accessToken
+  });
+
+  plus.people.get({
+  userId: 'me',
+  auth: oauth2Client
+  }, function (err, response) {
+    console.log("g+ err: " + JSON.stringify(err));
+    console.log("g+ response: " + JSON.stringify(response));
+    // handle err and response
+  });
+
+  // Fulfill action business logic
+  function responseHandler1 (app) {
+    // Complete your fulfillment logic and send a response
+    app.ask('Hello, World!');
   }
 
-  let actionMap = new Map();
-  actionMap.set("permsaction", permsRequest);
+  const actionMap = new Map();
+  actionMap.set('heyhey', responseHandler1);
+
   app.handleRequest(actionMap);
 };
-// });
+// [END YourAction]
 
-// server.listen(process.env.PORT || 8000, function() {
-//   console.log("Server listening");
-// });
+// "use strict";
+//
+// process.env.DEBUG = "actions-on-google:*";
+// const App = require("actions-on-google").ApiAiApp;
+// // const express = require("express");
+// // const bodyParser = require('body-parser');
+// //
+// // var server = express();
+// // server.use(bodyParser.json());
+//
+// // server.post("/", function(req, res) {
+//
+// exports.chromeControl = (req, res) => {
+//   const app = new App({req, res});
+//   console.log("res: " + res);
+//   console.log("Request headers: " + JSON.stringify(req.headers));
+//   console.log("Request body: " + JSON.stringify(req.body));
+//   console.log("req.body.result.action: "  + req.body.result.action);
+//   console.log("req.body.result.action == permsaction ? " + (req.body.result.action == "permsaction"));
+//
+//   function permsRequest (app) {
+//     console.log("got in func???");
+//     app.tell("webhook works!!!");
+//   }
+//
+//   let actionMap = new Map();
+//   actionMap.set("permsaction", permsRequest);
+//   app.handleRequest(actionMap);
+// };
+// // });
+//
+// // server.listen(process.env.PORT || 8000, function() {
+// //   console.log("Server listening");
+// // });
