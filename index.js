e2659f84eba3571f9066aecb78631e117279b84e
Dhanush Patel <dhanush.patel@ymail.com>
2017-08-05 01:17:31 -0700

    fixed chrome status bug
    was crashing when not logged into chrome for some reason, this should hopefully fix that

--- index.js
+++ index.js
@@ -123,6 +123,7 @@ exports.chromeControl = (request, response) => {
       console.log("snapshot.val()[" + gUser.id + "].chromeLoggedIn: " + snapshot.val()[gUser.id].chromeLoggedIn);
       if (!snapshot.val()[gUser.id].chromeLoggedIn) {
         displayMsg = "Hey! It seems like you haven't installed the \"Browser Control\" Chrome Extension in your Google Chrome browser yet. Please come back after you've done that.";
+        app.tell(displayMsg);
       } else {
         var gRef = admin.database().ref("users/" + gUser.id);
         var params = {"command": action};
