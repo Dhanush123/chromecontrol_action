b4bd018efd15b7bb88e262fdbbea878d3cd27b86
Dhanush Patel <dhanush.patel@ymail.com>
2017-08-18 02:44:48 -0700

    debug 1.1

--- index.js
+++ index.js
@@ -64,9 +64,9 @@ exports.chromeControl = (request, response) => {
   function accessTokenCheck(func1, func2, app) {
     reqnpm("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="+user.accessToken, function (error, response, body) {
       console.log('accessTokenCheck statusCode:', response && response.statusCode);
+      console.log('accessTokenCheck body:', body);
+      console.log('accessTokenCheck error:', error);
       if(error || body.expires_in < 300) {
-        console.log('accessTokenCheck error:', error);
-        console.log('accessTokenCheck body:', body);
         console.log("going to update access_token now!!!");
         accessTokenUpdate(func1, func2, app);
       }
